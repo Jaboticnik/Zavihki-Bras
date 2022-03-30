e763fc7f9dcdd2f51a83db474282169ad1fac402
Jaboticnik <78621415+Jaboticnik@users.noreply.github.com>
2022-03-30 21:28:20 +0200

    Posodobitev
    NE

--- index.js
+++ index.js
@@ -1,89 +1,89 @@
-let letmojizaznamki = []
+let mojizaznamki = [];
 
-Zaznamkishramba = localStorage.getItem("Zaznamki")
-const Predpomnjenipodatki = JSON.parse(Zaznamkishramba)
+Zaznamkishramba = localStorage.getItem("Zaznamki");
+const Predpomnjenipodatki = JSON.parse(Zaznamkishramba);
 
-const Gumbizbrisi = document.getElementById("izbrisi-gumb")
-const vnosuporabnika = document.getElementById("vnos-pr") 
-const VnosGumb = document.getElementById("vnos-gumb")
-const SeznamHTML = document.getElementById("seznamzaznamkov")
-const Shranizavihek = document.getElementById("zavihek-gumb")
-const Besedilo = document.getElementById("napisi")
+const Gumbizbrisi = document.getElementById("izbrisi-gumb");
+const vnosuporabnika = document.getElementById("vnos-pr") ;
+const VnosGumb = document.getElementById("vnos-gumb");
+const SeznamHTML = document.getElementById("seznamzaznamkov");
+const Shranizavihek = document.getElementById("zavihek-gumb");
+const Besedilo = document.getElementById("napisi");
 
-const ul = document.createElement("ul")
+const ul = document.createElement("ul");
 
 if (Zaznamkishramba) {
-    mojizaznamki = Predpomnjenipodatki
-    Prikaz(mojizaznamki)
+    mojizaznamki = Predpomnjenipodatki;
+    Prikaz(mojizaznamki);
 }
 
 Shranizavihek.addEventListener('click', 
 function(){
     browser.tabs.query({active: true, currentWindow: true}, function(tabs){
-        mojizaznamki.push(tabs[0].url)
-        localStorage.setItem("Zaznamki", JSON.stringify(mojizaznamki))
-        Prikaz(mojizaznamki)
-        console.log( localStorage.getItem("Zaznamki") )
-    })
-})
+        mojizaznamki.push(tabs[0].url);
+        localStorage.setItem("Zaznamki", JSON.stringify(mojizaznamki));
+        Prikaz(mojizaznamki);
+        console.log( localStorage.getItem("Zaznamki") );
+    });
+});
 
 function Prikaz(vrednost) {
-Besedilo.innerHTML = ""
-ul.innerHTML = ""
+Besedilo.innerHTML = "";
+ul.innerHTML = "";
 
 for (i = 0; i < vrednost.length; i++) {
 
-const li = document.createElement("li")   
+const li = document.createElement("li")  ;
 
-const a = document.createElement("a")
-a.setAttribute('href', `${vrednost[i]}`)
-a.setAttribute('target', '_blank')
-a.textContent = `${vrednost[i]}`
-li.appendChild(a)
+const a = document.createElement("a");
+a.setAttribute('href', `${vrednost[i]}`);
+a.setAttribute('target', '_blank');
+a.textContent = `${vrednost[i]}`;
+li.appendChild(a);
 
-const button = document.createElement("button")
-button.setAttribute('id', i)
-button.innerHTML= `<img src="slike/smetnjak.svg" style="height:13px">`
+const button = document.createElement("button");
+button.setAttribute('id', i);
+button.innerHTML= `<img alt="Izbrisi" src="slike/smetnjak.svg" style="height:13px">`;
 
-li.append(button)
- ul.setAttribute('id', "seznamzaznamkov")
- Besedilo.appendChild(ul)
- ul.appendChild(li)
+li.append(button);
+ ul.setAttribute('id', "seznamzaznamkov");
+ Besedilo.appendChild(ul);
+ ul.appendChild(li);
  function onClick() {
-    Izbrisi(button.id)
-    console.log("PRITISNJEN" + button.id)
+    Izbrisi(button.id);
+    console.log("PRITISNJEN" + button.id);
  }
  button.addEventListener("click", onClick, false);
 }}
 
-Gumbizbrisi.addEventListener('click', 
+Gumbizbrisi.addEventListener('click',
     function() {
-    localStorage.clear()
-    mojizaznamki = []
-    SeznamHTML.textContent = ''
-    Prikaz(mojizaznamki)
+    localStorage.clear();
+    mojizaznamki = [];
+    SeznamHTML.textContent = '';
+    Prikaz(mojizaznamki);
 
-})
+});
 
 
 VnosGumb.addEventListener("click", function(){
     if(vnosuporabnika.value === "") {
-        alert("Izpolni vsa polja")
+        alert("Izpolni vsa polja");
     }
     else{
-        const Vsebinavnosa = vnosuporabnika.value 
-        mojizaznamki.push(Vsebinavnosa)
-        localStorage.setItem("Zaznamki", JSON.stringify(mojizaznamki))
-        Prikaz(mojizaznamki)
-        vnosuporabnika.value = ""
-        console.log( localStorage.getItem("Zaznamki") )
+        const Vsebinavnosa = vnosuporabnika.value;
+        mojizaznamki.push(Vsebinavnosa);
+        localStorage.setItem("Zaznamki", JSON.stringify(mojizaznamki));
+        Prikaz(mojizaznamki);
+        vnosuporabnika.value = "";
+        console.log( localStorage.getItem("Zaznamki") );
     }
     
     
-})
+});
 
 function Izbrisi(vrednost) {
-    mojizaznamki.splice(vrednost, 1)
-    localStorage.setItem("Zaznamki", JSON.stringify(mojizaznamki))
-    Prikaz(mojizaznamki)
+    mojizaznamki.splice(vrednost, 1);
+    localStorage.setItem("Zaznamki", JSON.stringify(mojizaznamki));
+    Prikaz(mojizaznamki);
 }
